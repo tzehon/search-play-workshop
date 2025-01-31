d1bebf3fa25be62ffcb8e7c986abedec3558b9cf
Erik Hatcher <erik.hatcher@gmail.com>
2025-01-31 02:08:28 -0500

    Major checkpoint: many exercises added and a Playground component

---
+++ src/components/Playground/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "@site/src/components/Link";
+
+export default function Playground(props) {
+
+  // props.url: Full Playground URL, using the pieces and adding UTMs possibly
+  // props.pid: Playground ID
+
+  // TODO:
+  //   * Accept full playground url too, strip /.corp/ off
+  //   * Embed as iframe? (as well as link to)
+  //   * Add UTM support
+
+  const url = props.url || "https://search-playground.mongodb.com/tools/code-sandbox/snapshots/" + props.pid;
+
+  return (
+   <Link {...props} to={url} />
+    // <div>
+    //   Playground Link for this Exercise: <a href={url} target="_blank">Playground</a>
+
+    //   <hr />
+    //   <div>
+    //     {props.children}
+    //   </div>
+
+    // </div>
+  )
+}
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -3,10 +3,12 @@ import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Link from "@site/src/components/Link";
 import Screenshot from "@site/src/components/Screenshot";
+import Playground from "@site/src/components/Playground";
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   Link,
-  Screenshot
+  Screenshot,
+  Playground
 };
